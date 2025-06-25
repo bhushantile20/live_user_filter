6fa485331db6b035631c86d1d6109aa43a71fbfe
bhushantile20 <bhushantile2003@gmail.com>
2025-06-25 15:50:34 +0530

    Update learn.js
    updated
    
    Co-Authored-By: Bhushan Tile <2024.bhushan.tile@ves.ac.in>

--- learn.js
+++ learn.js
@@ -1,2 +1,19 @@
-------gfg
--- this is for the practice how things works 
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript Variables</h1>
+
+<p>In this example, x, y, and z are variables.</p>
+
+<p id="demo"></p>
+
+<script>
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML =
+"The value of z is: " + z;
+</script>
+
+</body>
+</html>
